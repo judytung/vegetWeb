e5bfc88d2757fd28549a24c32a8ae336c676784c
TUNG HSIAO HAN <sky250331@gmail.com>
2021-12-02 00:48:10 +0800

    種類篩選功能

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ let data = [];
 function getData () {
     axios.get(url).then(function (response) {
         data = response.data;
-        renderData();
+        // renderData();
     });
 }
 getData();
 
-function renderData () {
+function renderData (showData) {
     let str = '';
-    data.forEach((item) => {
+    showData.forEach((item) => {
         str += `<tr>
         <td>${item.作物名稱}</td>
         <td>${item.上價}</td>
@@ -30,3 +30,42 @@ function renderData () {
 }
 
 // 種類篩選，在 button 裡有埋 data-type
+// 監聽三個按鈕的大區塊
+const buttonGroup = document.querySelector(".button-group");
+
+buttonGroup.addEventListener('click',function (e) {
+    // 透過 if 判斷式判斷是否點到按鈕
+    if (e.target.nodeName === 'BUTTON') {
+        // 宣告這三個按鈕的節點，tabs 為類陣列
+        const tabs = document.querySelectorAll('.button-group button');
+        // 利用回圈拿掉按鈕的 active 樣式
+        tabs.forEach((item) => {
+            e.target.classList.remove('active');
+        });
+        // 取出埋在 html 裡的 data-type 的值賦予到變數 type 上
+        let type = e.target.dataset.type;
+        // 用 if 判斷式去判斷是 N04、N05、N06 哪一個，並在裡面用 filter 篩選資料再渲染出來
+        let filterData = [];
+        if (type === 'N04') {
+            // 宣告一個空陣列來存放篩選出來的資料
+            filterData = data.filter((item) => {
+                if (item.種類代碼 === 'N04') {
+                    return item;
+                }
+            });
+        } else if (type === 'N05') {
+            filterData = data.filter((item) => {
+                if (item.種類代碼 === 'N05') {
+                    return item;
+                }
+            });
+        } else if (type === 'N06') {
+            filterData = data.filter((item) => {
+                if (item.種類代碼 === 'N06') {
+                    return item;
+                }
+            });
+        };
+        renderData(filterData);
+    };
+})
